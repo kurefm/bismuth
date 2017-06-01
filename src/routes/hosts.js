81fa5fda263b20ba02dd47cbbf0c532ff0baf9cc
we4thy <lsj405@outlook.com>
2017-06-01 20:48:25 +0800

    Add hosts function

--- src/routes/hosts.js
+++ src/routes/hosts.js
@@ -5,4 +5,20 @@ router.get('/', (req, res) => {
   service.hosts().then(hosts => res.json({ hosts }));
 });
 
+router.get('/:ip', (req, res) => {
+  service.host(req.params.ip).then(host => res.json({ host }));
+});
+
+router.get('/:ip/ports', (req, res) => {
+  service.ports(req.params.ip).then(ports => res.json({ ports }));
+});
+
+router.get('/:ip/os-history', (req, res) => {
+  service.osHistory(req.params.ip).then(os => res.json({ os }));
+});
+
+router.get('/:ip/host-history', (req, res) => {
+  service.hostHistory(req.params.ip).then(os => res.json({ os }));
+});
+
 module.exports = router;
--- src/service/nmap/searcher.js
+++ src/service/nmap/searcher.js
@@ -63,6 +63,133 @@ function hosts() {
   });
 }
 
+function host(ip) {
+  return client.search({
+    index: 'nmap',
+    type: 'os',
+    body: {
+      "size": 1,
+      "query": {
+        "bool": {
+          "must": {
+            "term": { "ipv4.raw": { "value": ip } }
+          }
+        }
+      },
+      "sort": [
+        { "scan_at": "desc" }
+      ]
+    }
+  }).then(resp => get(resp, 'hits.hits[0]._source'));
+}
+
+function createPortsMsearchBody(ip,ports) {
+  let body = [];
+  ports.forEach(port => {
+    body.push({ index: 'nmap', type: 'port' });
+    body.push({
+      query: {
+        bool: {
+          must: [
+            {term: { 'ipv4.raw': ip }},
+            {term: { 'portid.raw': port }}
+          ]
+        }
+      },
+      size: 1,
+      sort: [{ scan_at: { order: 'desc' } }]
+    });
+  });
+  return body;
+}
+
+function ports(ip) {
+  return new Promise((resolve, reject) => {
+    waterfall([
+      callback => {
+        client.search({
+          index: 'nmap',
+          type: 'port',
+          body: {
+            "query": {
+              "bool": {
+                "must": {
+                  "term": { "ipv4.raw": { "value": ip }}
+                }
+              }
+            },
+            "aggs": {
+              "ports": {
+                "terms": { "field":	"portid.raw" }
+              }
+            },
+            "size": 0
+          }
+        }).then(
+          resp => callback(null, get(resp, 'aggregations.ports.buckets').map(ports => ports.key)),
+          callback
+          );
+      },
+      (ports, callback) => {
+        client.msearch({ body: createPortsMsearchBody(ip, ports) }).then(
+          ({ responses }) => callback(null, responses.map(resp => merge(
+            get(resp, 'hits.hits[0]._source'),
+            { total: get(resp, 'hits.total') }
+          ))),
+          callback
+        );
+      }
+    ], (error, ports) => {
+      if (error) reject(error);
+      resolve(ports);
+    });
+  });
+}
+
+function osHistory(ip) {
+    return client.search({
+    index: 'nmap',
+    type: 'os',
+    body: {
+      "size": 10000,
+      "query": {
+        "bool": {
+          "must": [
+            {"term": { "ipv4.raw":  { "value": ip }}}
+          ]
+        }
+      },
+      "sort": [
+        { "scan_at": "desc" }
+      ]
+    }
+  }).then(resp => get(resp, 'hits.hits').map(hits => hits._source));
+}
+
+function hostHistory(ip) {
+    return client.search({
+    index: 'nmap',
+    type: 'host',
+    body: {
+      "size": 10000,
+      "query": {
+        "bool": {
+          "must": [
+            {"term": { "ipv4.raw":  { "value": ip }}}
+          ]
+        }
+      },
+      "sort": [
+        { "scan_at": "desc" }
+      ]
+    }
+  }).then(resp => get(resp, 'hits.hits').map(hits => hits._source));
+}
+
 module.exports = {
-  hosts
+  hosts,
+  host,
+  ports,
+  osHistory,
+  hostHistory
 };
